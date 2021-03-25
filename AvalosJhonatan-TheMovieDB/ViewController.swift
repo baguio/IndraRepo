cd818c8dc96eb2f4477a28d922b3fa01fa97e354
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-25 11:36:07 -0500

    Initial Commit

---
+++ AvalosJhonatan-TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvalosJhonatan-TheMovieDB
+//
+//  Created by Jhonatan A. on 25/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
