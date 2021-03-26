d221917b24a091e246cb09e0ad9bb434958626c2
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-25 23:05:50 -0500

    MovieList: Added simple table view cell class
    Used default style

---
+++ TheMovieDB/MovieList/View/MovieListCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListCell.swift
+//  TheMovieDB
+//
+//  Created by Jhonatan A. on 25/03/21.
+//
+
+import UIKit
+
+class MovieListCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
