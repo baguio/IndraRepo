5333e25f8b811712e460d95b26a5dcba9b75dc97
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-25 23:13:31 -0500

    MovieDetail: ViewModel can obtain a movie poster

--- TheMovieDB/MovieDetail/Logic/MovieDetailViewModel.swift
+++ TheMovieDB/MovieDetail/Logic/MovieDetailViewModel.swift
@@ -10,8 +10,13 @@ import Combine
 
 class MovieDetailViewModel {
     let movie: Movie
+    let logic = MovieDetailLogic()
     
     init(movie: Movie) {
         self.movie = movie
+    }
+    
+    func obtainPoster() -> AnyPublisher<Data, Swift.Error>{
+        logic.obtainPoster(for: movie)
     }
 }
