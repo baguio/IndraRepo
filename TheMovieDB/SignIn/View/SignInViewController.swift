994a75cadfba044c9ac380af7a73078b1a151e43
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-25 22:43:52 -0500

    SignIn: Form connected to session manager
    SignIn dismisses itself, after being presented, when a session is obtained. Missing session error visual cues

--- TheMovieDB/SignIn/View/SignInViewController.swift
+++ TheMovieDB/SignIn/View/SignInViewController.swift
@@ -11,6 +11,24 @@ import Combine
 class SignInViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    let model = SignInViewModel()
+    
+    var signInCancellable: AnyCancellable?
+
     @IBAction func signInActionTriggered(_ sender: Any) {
+        signInCancellable = model.signIn(
+            withUser: usernameTextField.text,
+            password: passwordTextField.text
+        ).sink { (completion) in
+            if case .failure(let error) = completion {
+                print("Error! \(error)")
+                // TODO: UI error
+            }
+        } receiveValue: { (_) in
+            self.dismiss(
+                animated: true,
+                completion: nil
+            )
+        }
     }
 }
