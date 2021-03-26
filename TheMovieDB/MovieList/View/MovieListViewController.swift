6ef09b53e56e5c250babd53211f6400650912d1a
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-25 22:16:26 -0500

    MovieList: Design added
    Also, updated to be shown as first screen

---
+++ TheMovieDB/MovieList/View/MovieListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MovieListViewController.swift
+//  TheMovieDB
+//
+//  Created by Jhonatan A. on 25/03/21.
+//
+
+import UIKit
+import Combine
+
+class MovieListViewController: UIViewController {
+}
