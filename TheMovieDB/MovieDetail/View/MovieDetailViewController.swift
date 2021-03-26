7becc22afb739f465e773a7d19fa5af6d20997b9
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-25 22:57:13 -0500

    MovieDetail: Design added
    Included class and outlets

---
+++ TheMovieDB/MovieDetail/View/MovieDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailViewController.swift
+//  TheMovieDB
+//
+//  Created by Jhonatan A. on 25/03/21.
+//
+
+import UIKit
+import UICircularProgressRingLegacy
+import Combine
+
+class MovieDetailViewController: UIViewController {
+    @IBOutlet weak var circleViewHolder: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBAction func closeButtonTriggered(_ sender: Any) {
+    }
+}
