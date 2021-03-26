e7f1482f58cd2909fbff4f1f96e0531a94fce02d
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-25 22:20:40 -0500

    SignIn: Design added
    Also, fixed bad file references at pbxproj

---
+++ TheMovieDB/SignIn/View/SignInViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  AvalosJhonatan-TheMovieDB
+//
+//  Created by Jhonatan A. on 25/03/21.
+//
+
+import UIKit
+import Combine
+
+class SignInViewController: UIViewController {
+}
