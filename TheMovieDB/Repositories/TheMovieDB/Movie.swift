fe79eb66518ac7a0c2f76e2d693bf550c53cd2fd
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-26 23:06:05 -0500

    Movie: Added helper constructor based on LocalMovie
    CoreData related

--- TheMovieDB/Repositories/TheMovieDB/Movie.swift
+++ TheMovieDB/Repositories/TheMovieDB/Movie.swift
@@ -14,3 +14,26 @@ struct Movie: Codable {
     let title: String
     let vote_average: Double
 }
+
+extension Movie {
+    static func fromLocal(_ movie: LocalMovie) -> Movie? {
+        let vote_average = movie.vote_average
+        guard
+            let title = movie.title,
+            let overview = movie.overview,
+            let poster_path = movie.poster_path,
+            let release_date = movie.release_date
+        else {
+            assertionFailure("Should not be empty")
+            return nil
+        }
+        
+        return Movie(
+            overview: overview,
+            poster_path: poster_path,
+            release_date: release_date,
+            title: title,
+            vote_average: vote_average
+        )
+    }
+}
