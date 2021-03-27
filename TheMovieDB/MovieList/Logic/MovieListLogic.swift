6d85d0d89545c7e34d5e4c3e40ac479b72648662
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-26 23:12:29 -0500

    MovieList: Logic now checks for a session

--- TheMovieDB/MovieList/Logic/MovieListLogic.swift
+++ TheMovieDB/MovieList/Logic/MovieListLogic.swift
@@ -25,15 +25,39 @@ extension MovieListLogicProtocol {
 
 class MovieListLogic: MovieListLogicProtocol {
     let remoteManager = TMDBRemoteManager()
+    let localManager = TMDBLocalManager()
     let sessionManager = SessionManager()
     
     func obtainSession() -> AnyPublisher<Session, SessionManager.PersistedSessionError> {
         SessionManager().obtainPersistedSession()
     }
     
-    func obtainUpcomingMovies(atPage page: Int = 1) -> AnyPublisher<[Movie], Swift.Error> {
-        remoteManager.obtainUpcomingMovies(at: page)
-            .map { $0.results }
+    func obtainUpcomingMovies(atPage page: Int) -> AnyPublisher<[Movie], Swift.Error> {
+        enum RemoteOrLocalMovies {
+            case local([LocalMovie])
+            case remote([Movie])
+        }
+        
+        return localManager.retrieveMovies(forPage: page)
+            .map { $0 as [LocalMovie]? }
+            .replaceError(with: nil)
+            .flatMap { localMovies -> AnyPublisher<[Movie], Swift.Error> in
+                if let localMovies = localMovies {
+                    return Just<[Movie]>(
+                        localMovies.compactMap { Movie.fromLocal($0) }
+                    )
+                    .mapError { $0 as Swift.Error }
+                    .eraseToAnyPublisher()
+                } else {
+                    return self.remoteManager.obtainUpcomingMovies(at: page)
+                        .flatMap { movies -> AnyPublisher<[Movie], Swift.Error> in
+                            self.localManager.saveUpcomingMovies(movies)
+                                .map { movies.results }
+                                .eraseToAnyPublisher()
+                        }
+                        .eraseToAnyPublisher()
+                }
+            }
             .eraseToAnyPublisher()
     }
 }
