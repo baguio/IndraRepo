e0c3bb7269becbc6328ba96d237a07851a0cc574
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-26 23:19:20 -0500

    Tests: RemoteTMDB placeholder
    Technical debt, implies using URLSession through protocols or any helper that enables mocking

---
+++ TheMovieDBTests/RemoteTMDBTest.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteTMDBTest.swift
+//  TheMovieDBTests
+//
+//  Created by Jhonatan A. on 26/03/21.
+//
+
+import XCTest
+
+class RemoteTMDBTest: XCTestCase {
+    // TODO. A protocol-oriented URLSession handling would be required
+}
