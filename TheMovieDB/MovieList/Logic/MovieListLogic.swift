98a5882e928e815a289a5b8b4316c533b219f541
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-03-26 23:32:11 -0500

    MovieList: Disabled Local storage temporarily
    This way the scene flow works and it's demoable

--- TheMovieDB/MovieList/Logic/MovieListLogic.swift
+++ TheMovieDB/MovieList/Logic/MovieListLogic.swift
@@ -38,26 +38,25 @@ class MovieListLogic: MovieListLogicProtocol {
             case remote([Movie])
         }
         
-        return localManager.retrieveMovies(forPage: page)
-            .map { $0 as [LocalMovie]? }
-            .replaceError(with: nil)
-            .flatMap { localMovies -> AnyPublisher<[Movie], Swift.Error> in
-                if let localMovies = localMovies {
-                    return Just<[Movie]>(
-                        localMovies.compactMap { Movie.fromLocal($0) }
-                    )
-                    .mapError { $0 as Swift.Error }
-                    .eraseToAnyPublisher()
-                } else {
+//        return localManager.retrieveMovies(forPage: page)
+//            .map { $0 as [LocalMovie]? }
+//            .replaceError(with: nil)
+//            .flatMap { localMovies -> AnyPublisher<[Movie], Swift.Error> in
+//                if let localMovies = localMovies {
+//                    return Just<[Movie]>(
+//                        localMovies.compactMap { Movie.fromLocal($0) }
+//                    )
+//                    .mapError { $0 as Swift.Error }
+//                    .eraseToAnyPublisher()
+//                } else {
                     return self.remoteManager.obtainUpcomingMovies(at: page)
-                        .flatMap { movies -> AnyPublisher<[Movie], Swift.Error> in
-                            self.localManager.saveUpcomingMovies(movies)
-                                .map { movies.results }
-                                .eraseToAnyPublisher()
-                        }
+//                        .flatMap { movies -> AnyPublisher<[Movie], Swift.Error> in
+//                            self.localManager.saveUpcomingMovies(movies)
+//                        }
+                        .map { $0.results }
                         .eraseToAnyPublisher()
-                }
-            }
-            .eraseToAnyPublisher()
+//                }
+//            }
+//            .eraseToAnyPublisher()
     }
 }
